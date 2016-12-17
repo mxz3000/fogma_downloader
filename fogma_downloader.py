a6d21edb0632e4c699ce8c701b0607dcbdeced98
Guillaume <rameg@neuf.fr>
2016-12-17 12:31:57 +0000

    script sorts pdfs into folders

--- fogma_downloader.py
+++ fogma_downloader.py
@@ -16,23 +16,38 @@ if not os.path.exists(directory):
     os.makedirs(directory)
     
     
-#get filenames from fogma and format them
+#get html from fogma and extract filenames
+#build folder path for each filename 
+curdir = ""
+curclass = ""
+dirs = []
 
 for line in f:
+	if ' Year": [{' in line:
+		curdir = line.strip()[1:-5]
+		if not os.path.exists(directory + '/' + curdir):
+			os.makedirs(directory + '/' + curdir)
+		
+	if '"title": ' in line:
+		curclass = line.strip()[10:-2]
+		if not os.path.exists(directory + '/' + curdir + '/' + curclass):
+			os.makedirs(directory + '/' + curdir + '/' + curclass)
+		
 	if '"link":' in line:
 		fname = line.strip()[9:-2]
 		filenames.append(fname)
+		dirs.append(directory + '/' + curdir + '/' + curclass + '/')
 		
 		
 #go thru filenames and download the pdfs from fogma
 
 print("downloading pdfs from fogma")
 
-for filename in filenames:
+for filename, path in zip(filenames,dirs):
     	full_url = "http://" + urllib2.quote(URL + filename)
     	print(filename)
 
-    	with file(directory + "/" + filename, 'wb') as outfile:
+    	with file(path + filename, 'wb') as outfile:
        		response = session.get(full_url, headers=HEADERS)
        		if not response.ok:
       			break
